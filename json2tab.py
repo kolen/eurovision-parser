fe237773040743abf4aac8a26d3975507f607d99
Konstantin Mochalov <incredible.angst@gmail.com>
2011-09-22 21:13:44 +0400

    More processing

--- json2tab.py
+++ json2tab.py
@@ -1,4 +1,4 @@
-from json import load
+from json import load, dumps
 from sys import stdin, stdout, argv
 
 data = load(open(argv[1]))
@@ -8,6 +8,14 @@ keys.sort()
 stdout.write("%s\n" % "\t".join(keys))
 
 for row in data:
-    rows = [unicode(row.get(key, '')).replace("\t", " ").encode('utf-8') for key in keys]
+    rows = []
+    for key in keys:
+        item = row.get(key, '')
+        if isinstance(item, dict) or isinstance(item, list) or isinstance(item, tuple):
+            item = dumps(item)
+
+        item = unicode(item).replace(u"\t", u" ").encode("utf-8")
+        rows.append(item)
+
     stdout.write("\t".join(rows))
     stdout.write("\n")
